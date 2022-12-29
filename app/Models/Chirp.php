582ae44a331970229f54be15c31dd5129f866366
wcliao1962 <wcliao1962@gmail.com>
2022-12-29 14:49:47 +0800

    Create an event & Dispatching the event

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,6 +13,10 @@ class Chirp extends Model
         'message'
     ];
 
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+    
     public function user()
     {
         return $this->belongsTo(User::class);
